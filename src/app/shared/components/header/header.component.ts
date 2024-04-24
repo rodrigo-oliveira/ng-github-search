d5a57d84eb3045a7a73784a9cd65fe675a586aa4
Rodrigo Oliveira <rodrigooliveira@Rodrigos-MacBook-Pro.local>
2024-04-24 19:24:16 -0300

    Added properties to header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { IconResolver, MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ MatIconModule ],
+  imports: [ CommonModule, MatIconModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Input() alignCenter = true;
+  @Input() showBackButton = false;
 
+  constructor(private router: Router) {}
+
+  back() {
+    this.router.navigate(['..']);
+  }
 }
