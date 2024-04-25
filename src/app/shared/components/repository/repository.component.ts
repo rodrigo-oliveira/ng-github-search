d7c0414799d6da5370bab15b54b8c0a8bf6da7b9
Rodrigo Oliveira <rodrigooliveira@Rodrigos-MacBook-Pro.local>
2024-04-25 15:46:54 -0300

    Added fork icon

--- src/app/shared/components/repository/repository.component.ts
+++ src/app/shared/components/repository/repository.component.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { GitHubRepository } from '../../../core/models/github-repository.interface';
 import { BadgeComponent } from '../badge/badge.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-repository',
   standalone: true,
   imports: [
     CommonModule,
-    BadgeComponent
+    BadgeComponent,
+    MatIconModule
   ],
   templateUrl: './repository.component.html',
   styleUrl: './repository.component.scss'
